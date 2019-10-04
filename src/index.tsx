a666ee570e8ac43d142cfe74f9ecb6fb6a7ac0d7
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2019-10-04 12:06:12 -0500

    export all

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,1 @@
-import * as React from 'react';
-import { Greeting } from './Greeting.gen';
-
-// Delete me
-export const Thing = () => {
-  return (
-    <div>
-      <Greeting name="Jared" /> the snozzberries taste like snozzberries
-    </div>
-  );
-};
+export * from './Greeting.gen';
